c5a01e04e9e94fad4af205bd55785eb0346d54c4
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-06 23:22:20 +0530

    Adding Claims and Signing the Token with Secret Key

--- main.go
+++ main.go
@@ -11,10 +11,16 @@ func main() {
 	// create token
 	token := jwt.New(jwt.SigningMethodHS256)
 
+	// print token
+	fmt.Println("Token just after creating =>", token)
+
 	// set claims
 	claims := token.Claims.(jwt.MapClaims)
 	claims["username"] = "tufan_ray"
 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	// print token
+	fmt.Println("Token after adding claim =>", token)
 
 	// sign token with secret key
 	tokenString, err := token.SignedString([]byte("my_secret_key"))
@@ -24,5 +30,5 @@ func main() {
 	}
 
 	// print token
-	fmt.Println(tokenString)
+	fmt.Println("Token after adding claim and signing with secret =>", tokenString)
 }
