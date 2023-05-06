9c17546b0a555484258806901a444a77764ad2e9
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-06 23:13:48 +0530

    Generating Simple JWT Token and Printing

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/sraynitjsr
+
+go 1.20
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 func main() {
-  fmt.Println("JWT Auth Using Golang") 
+	// create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = "tufan_ray"
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	// sign token with secret key
+	tokenString, err := token.SignedString([]byte("my_secret_key"))
+	if err != nil {
+		fmt.Println("Error generating token string:", err)
+		return
+	}
+
+	// print token
+	fmt.Println(tokenString)
 }
